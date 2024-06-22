fc352374c8b52dfb4b496bb22fa0acafabab1d6e
notime2d8 <notime2d8@gmail.com>
2024-06-22 12:38:55 -0500

    Add files via upload

---
+++ device/rockchip/common/build/rockchip/Partitions.mk
@@ -0,0 +1,88 @@
+#
+# Copyright 2021 Rockchip Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+ifeq ($(PRODUCT_USE_DYNAMIC_PARTITIONS), true)
+ROCKCHIP_READ_ONLY_FILE_SYSTEM_TYPE ?= ext4
+endif
+
+BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := $(ROCKCHIP_READ_ONLY_FILE_SYSTEM_TYPE)
+
+# Add standalone vendor partition configrations
+TARGET_COPY_OUT_VENDOR := vendor
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := $(ROCKCHIP_READ_ONLY_FILE_SYSTEM_TYPE)
+
+# Add standalone odm partition configrations
+TARGET_COPY_OUT_ODM := odm
+BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := $(ROCKCHIP_READ_ONLY_FILE_SYSTEM_TYPE)
+
+TARGET_USERIMAGES_USE_EXT4 ?= true
+TARGET_USERIMAGES_USE_F2FS ?= false
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE ?= ext4
+
+# use ext4 cache for OTA
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE ?= ext4
+# Add standalone metadata partition
+BOARD_USES_METADATA_PARTITION ?= true
+
+#Calculate partition size from parameter.txt
+USE_DEFAULT_PARAMETER := $(shell test -f $(TARGET_DEVICE_DIR)/parameter.txt && echo true)
+ifeq ($(strip $(USE_DEFAULT_PARAMETER)), true)
+  ifeq ($(PRODUCT_USE_DYNAMIC_PARTITIONS), true)
+    BOARD_SUPER_PARTITION_SIZE := $(shell python device/rockchip/common/get_partition_size.py $(TARGET_DEVICE_DIR)/parameter.txt super)
+    BOARD_ROCKCHIP_DYNAMIC_PARTITIONS_SIZE := $(shell expr $(BOARD_SUPER_PARTITION_SIZE) - 4194304)
+  else
+    BOARD_SYSTEMIMAGE_PARTITION_SIZE := $(shell python device/rockchip/common/get_partition_size.py $(TARGET_DEVICE_DIR)/parameter.txt system)
+    BOARD_VENDORIMAGE_PARTITION_SIZE := $(shell python device/rockchip/common/get_partition_size.py $(TARGET_DEVICE_DIR)/parameter.txt vendor)
+    BOARD_ODMIMAGE_PARTITION_SIZE := $(shell python device/rockchip/common/get_partition_size.py $(TARGET_DEVICE_DIR)/parameter.txt odm)
+  endif
+  BOARD_CACHEIMAGE_PARTITION_SIZE := $(shell python device/rockchip/common/get_partition_size.py $(TARGET_DEVICE_DIR)/parameter.txt cache)
+  BOARD_BOOTIMAGE_PARTITION_SIZE := $(shell python device/rockchip/common/get_partition_size.py $(TARGET_DEVICE_DIR)/parameter.txt boot)
+  BOARD_DTBOIMG_PARTITION_SIZE := $(shell python device/rockchip/common/get_partition_size.py $(TARGET_DEVICE_DIR)/parameter.txt dtbo)
+  BOARD_RECOVERYIMAGE_PARTITION_SIZE := $(shell python device/rockchip/common/get_partition_size.py $(TARGET_DEVICE_DIR)/parameter.txt recovery)
+  # Header V3, add vendor_boot
+  ifeq ($(BOARD_BUILD_GKI),true)
+    BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := $(shell python device/rockchip/common/get_partition_size.py $(TARGET_DEVICE_DIR)/parameter.txt vendor_boot)
+  endif
+  #$(info Calculated BOARD_SYSTEMIMAGE_PARTITION_SIZE=$(BOARD_SYSTEMIMAGE_PARTITION_SIZE) use $(TARGET_DEVICE_DIR)/parameter.txt)
+else
+  ifeq ($(PRODUCT_USE_DYNAMIC_PARTITIONS), true)
+    ifeq ($(BUILD_WITH_GO_OPT), true)
+      BOARD_SUPER_PARTITION_SIZE ?= 2516582400
+    else
+      BOARD_SUPER_PARTITION_SIZE ?=  10737418240
+#      BOARD_SUPER_PARTITION_SIZE ?=  3263168512
+    endif
+    BOARD_ROCKCHIP_DYNAMIC_PARTITIONS_SIZE ?= $(shell expr $(BOARD_SUPER_PARTITION_SIZE) - 4194304)
+  else
+    BOARD_SYSTEMIMAGE_PARTITION_SIZE ?= 2726297600
+    BOARD_VENDORIMAGE_PARTITION_SIZE ?= 536870912
+    BOARD_ODMIMAGE_PARTITION_SIZE ?= 134217728
+  endif
+  BOARD_CACHEIMAGE_PARTITION_SIZE ?= 402653184
+  BOARD_RECOVERYIMAGE_PARTITION_SIZE ?= 100663296
+  BOARD_DTBOIMG_PARTITION_SIZE ?= 4194304
+  # Header V3, add vendor_boot
+  ifeq ($(BOARD_BUILD_GKI),true)
+    BOARD_BOOTIMAGE_PARTITION_SIZE ?= 67108864
+    BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE ?= 41943040
+    BOARD_RESOURCEIMAGE_PARTITION_SIZE ?= 16777216
+  else
+    BOARD_BOOTIMAGE_PARTITION_SIZE ?= 67108864
+  endif
+  ifneq ($(strip $(TARGET_DEVICE_DIR)),)
+    #$(info $(TARGET_DEVICE_DIR)/parameter.txt not found! Use default BOARD_SYSTEMIMAGE_PARTITION_SIZE=$(BOARD_SYSTEMIMAGE_PARTITION_SIZE))
+  endif
+endif
